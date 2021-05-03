b7b48f89681cb5e4b568d8ed4e33bac9307c0491
spuyelli <spuyelli@gmail.com>
2021-05-02 22:31:29 -0300

    Inicio del curso de HERENCIA. Se crea clase padre

---
+++ Inheritance.kt
@@ -0,0 +1,13 @@
+fun main() {
+    
+    
+}
+
+abstract class Dwelling(private var residents: Int) {
+	abstract val buildingMaterial: String
+    abstract val capacity: Int
+    
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+}
