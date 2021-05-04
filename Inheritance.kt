7032eb6e817b25cc1c32d00fc52ee69ce063e379
spuyelli <spuyelli@gmail.com>
2021-05-03 21:38:47 -0300

    Probando la funcionalidad de getRoom()

--- Inheritance.kt
+++ Inheritance.kt
@@ -21,6 +21,9 @@ fun main() {
         println("Capacity: ${capacity}")
         println("Material: ${buildingMaterial}")
         println("Has room? ${hasRoom()}")
+        getRoom()
+        println("Has room? ${hasRoom()}")
+        getRoom()
         println("Floor area: %.2f".format(floorArea()))
     }
 
