b9b72e85c1145e3277a8da4a84acbb3f0a3526fe
spuyelli <spuyelli@gmail.com>
2021-05-03 21:35:03 -0300

    Se agrega funcionalidad para sumar residentes

--- Inheritance.kt
+++ Inheritance.kt
@@ -45,6 +45,16 @@ abstract class Dwelling(private var residents: Int) {
     }
 
     abstract fun floorArea(): Double
+
+    fun getRoom(){
+        if(capacity>residents){
+            residents++
+            println("You got a room!")
+        }
+        else{
+            println("Sorry, at capacity and no rooms left.")
+        }
+    }
 }
 
 class SquareCabin(residents: Int, val length: Double) : Dwelling(residents){
