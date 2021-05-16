f42f016529dc3826fcf21ad3e34fe6533c099f01
spuyelli <spuyelli@gmail.com>
2021-05-16 00:10:25 -0300

    Empezando a trabajar con listas editables

---
+++ MutableList.kt
@@ -0,0 +1,7 @@
+// https://developer.android.com/training/kotlinplayground
+
+
+fun main() {
+    val entradas = mutableListOf<String>()
+    //val entradas: MutableList<String> = mutableListOf()     // Otra forma correcta de inicializar la lista vacía
+}
